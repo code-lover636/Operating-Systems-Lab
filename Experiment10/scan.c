bc62ef367d7c89db0923def17c1ea74a46b52bff
code-lover636 <aravindashokan636@gmail.com>
2024-05-30 18:53:22 +0530

    Experiment 10

---
+++ Experiment10/scan.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int scanShedule(int no_of_requests, int requests[no_of_requests], int current){
+    int seek_time = 0, start_index=0, counter;
+
+    while(requests[start_index]<=current)
+        start_index++;
+    counter = start_index;
+
+    while(counter>0){
+        counter--;
+        seek_time += abs(requests[counter] - current);
+        current = requests[counter];
+    }
+    while(start_index < no_of_requests){
+        seek_time += abs(requests[start_index] - current);
+        current = requests[start_index];
+        start_index++;
+    }
+
+    return seek_time;
+}
+
+int main() {
+    int no_of_requests, current;
+    printf("Enter the number of disk requests: ");
+    scanf("%d", &no_of_requests);
+    no_of_requests += 1;
+    int requests[no_of_requests];
+    requests[0] = 0;
+
+    printf("Enter initial head position: ");
+    scanf("%d", &current);
+
+    printf("Enter %d disk requests: ", no_of_requests-1);
+    for (int i = 1; i < no_of_requests; i++)
+        scanf("%d", &requests[i]);
+    
+    // Sort the request array
+    for (int i=0; i < no_of_requests - 1; i++) {
+        for (int j = 0; j < no_of_requests - i - 1; j++) {
+            if (requests[j] > requests[j + 1]) {
+                int temp = requests[j];
+                requests[j] = requests[j + 1];
+                requests[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("No. of head movement = %d\n", scanShedule(no_of_requests, requests, current));
+}
+
